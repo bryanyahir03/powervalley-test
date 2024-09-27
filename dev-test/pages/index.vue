6b8f34673849046bb2643242f91b628f7ad46ec7
Chris Alexander Andino Godoy <46337972+chris43123@users.noreply.github.com>
2024-09-26 22:29:09 -0600

    Refactor app.vue and add new pages
    - Removed unused components NuxtRouteAnnouncer and NuxtWelcome from app.vue
    - Added new page components NuxtPage, index.vue, and tasks/index.vue
    - Updated app.vue to use NuxtPage component instead of the removed components
    - Created index.vue with a simple "Helloooooo world!" message
    - Created tasks/index.vue with a list of tasks and an "Add Task" button
    - Implemented addTask method to add new tasks to the list

--- dev-test/pages/index.vue
+++ dev-test/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtPage/>
+    Helloooooo world!
   </div>
 </template>
---
+++ dev-test/pages/tasks/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Tasks</h1>
+        <ul>
+        <li v-for="task in tasks" :key="task.id">
+            {{ task.title }}
+        </li>
+        </ul>
+        <button @click="addTask">Add Task</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tasks: [
+                
+            ]
+        }
+    },
+    methods:{
+        addTask(){
+            this.tasks.push({
+                id: this.tasks.length + 1,
+                title: 'Task ' + (this.tasks.length + 1)
+            })
+        }
+    }
+}
+</script>
